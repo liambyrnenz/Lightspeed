5bc57e8e82627ce3c9138f42caecdbeee3ac5cfe
Liam Byrne <liam.jb1@gmail.com>
2024-06-15 15:35:30 +0100

    Initial Commit

---
+++ Lightspeed/LightspeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightspeedApp.swift
+//  Lightspeed
+//
+//  Created by Liam on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LightspeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
